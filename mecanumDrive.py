97f8b9bfa5584b2c88b97326c1d143b69fd65909
Teemu Laurila <laurilateemu8@gmail.com>
2020-10-21 16:12:30 +0300

    Update code for arduino

--- mecanumDrive.py
+++ mecanumDrive.py
@@ -1,4 +1,4 @@
-from motorControl import motorControl
+from serialMotorControl import motorControl
 
 # Keyboard
 import tty
@@ -9,7 +9,7 @@ orig_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin)
 key = 0
 oldKey = 0
-speed = 100
+speed = 255
 
 m = motorControl()
 m.begin()
@@ -17,88 +17,91 @@ m.begin()
 print('Press ESC to exit')
 
 while key != chr(27): #ESC
-	key = sys.stdin.read(1)[0]
 
-	if key != oldKey:
+    key = sys.stdin.read(1)[0]
+    m.run()
+        
+    if key != oldKey:
 
-		# Stop
-		if key == 's':
-			m.setSpeed(0, 0)
-			m.setSpeed(1, 0)
-			m.setSpeed(2, 0)
-			m.setSpeed(3, 0)
+        # Stop
+        if key == 's':
+            m.setSpeed(0, 0)
+            m.setSpeed(1, 0)
+            m.setSpeed(2, 0)
+            m.setSpeed(3, 0)
 
-		# Move forward
-		if key == 'w':
-			m.setSpeed(0, speed)
-			m.setSpeed(1, speed)
-			m.setSpeed(2, speed)
-			m.setSpeed(3, speed)
 
-		# Move backwards
-		if key == 'x':
-			m.setSpeed(0, -speed)
-			m.setSpeed(1, -speed)
-			m.setSpeed(2, -speed)
-			m.setSpeed(3, -speed)
+        # Move forward
+        if key == 'w':
+            m.setSpeed(0, speed)
+            m.setSpeed(1, speed)
+            m.setSpeed(2, speed)
+            m.setSpeed(3, speed)
 
-		# Move forward at 45 degree angle to the right
-		if key == 'e':
-			m.setSpeed(0, speed)
-			m.setSpeed(1, 0)
-			m.setSpeed(2, speed)
-			m.setSpeed(3, 0)
+        # Move backwards
+        if key == 'x':
+            m.setSpeed(0, -speed)
+            m.setSpeed(1, -speed)
+            m.setSpeed(2, -speed)
+            m.setSpeed(3, -speed)
 
-		# Move backwards at 45 degree angle to the left
-		if key == 'z':
-			m.setSpeed(0, -speed)
-			m.setSpeed(1, 0)
-			m.setSpeed(2, -speed)
-			m.setSpeed(3, 0)
+        # Move forward at 45 degree angle to the right
+        if key == 'e':
+            m.setSpeed(0, speed)
+            m.setSpeed(1, 0)
+            m.setSpeed(2, speed)
+            m.setSpeed(3, 0)
 
-		# Move forward at 45 degree angle to the left
-		if key == 'q':
-			m.setSpeed(0, 0)
-			m.setSpeed(1, speed)
-			m.setSpeed(2, 0)
-			m.setSpeed(3, speed)
+        # Move backwards at 45 degree angle to the left
+        if key == 'z':
+            m.setSpeed(0, -speed)
+            m.setSpeed(1, 0)
+            m.setSpeed(2, -speed)
+            m.setSpeed(3, 0)
 
-		# Move backwards at 45 degree angle to the right
-		if key == 'c':
-			m.setSpeed(0, 0)
-			m.setSpeed(1, -speed)
-			m.setSpeed(2, 0)
-			m.setSpeed(3, -speed)
+        # Move forward at 45 degree angle to the left
+        if key == 'q':
+            m.setSpeed(0, 0)
+            m.setSpeed(1, speed)
+            m.setSpeed(2, 0)
+            m.setSpeed(3, speed)
 
-		# Strafe right
-		if key == 'd':
-			m.setSpeed(0, speed)
-			m.setSpeed(1, -speed)
-			m.setSpeed(2, speed)
-			m.setSpeed(3, -speed)
+        # Move backwards at 45 degree angle to the right
+        if key == 'c':
+            m.setSpeed(0, 0)
+            m.setSpeed(1, -speed)
+            m.setSpeed(2, 0)
+            m.setSpeed(3, -speed)
 
-		# Strafe left
-		if key == 'a':
-			m.setSpeed(0,- speed)
-			m.setSpeed(1, speed)
-			m.setSpeed(2, -speed)
-			m.setSpeed(3, speed)
+        # Strafe right
+        if key == 'd':
+            m.setSpeed(0, speed)
+            m.setSpeed(1, -speed)
+            m.setSpeed(2, speed)
+            m.setSpeed(3, -speed)
 
-		# Turn left
-		if key == 'h':
-			m.setSpeed(0, speed)
-			m.setSpeed(1, speed)
-			m.setSpeed(2, -speed)
-			m.setSpeed(3, -speed)
+        # Strafe left
+        if key == 'a':
+            m.setSpeed(0,- speed)
+            m.setSpeed(1, speed)
+            m.setSpeed(2, -speed)
+            m.setSpeed(3, speed)
 
-		# Turn right
-		if key == 'g':
-			m.setSpeed(0, -speed)
-			m.setSpeed(1, -speed)
-			m.setSpeed(2, speed)
-			m.setSpeed(3, speed)
+        # Turn left
+        if key == 'h':
+            m.setSpeed(0, speed)
+            m.setSpeed(1, speed)
+            m.setSpeed(2, -speed)
+            m.setSpeed(3, -speed)
 
-		oldKey = key
+        # Turn right
+        if key == 'g':
+            m.setSpeed(0, -speed)
+            m.setSpeed(1, -speed)
+            m.setSpeed(2, speed)
+            m.setSpeed(3, speed)
+
+        oldKey = key
 
 m.setSpeed(0, 0)
 m.setSpeed(1, 0)
